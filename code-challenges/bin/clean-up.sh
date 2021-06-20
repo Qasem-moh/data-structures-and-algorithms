dfbbccfe0e01bf050e875462f9ade3038a8dc6ce
qasem-moh <2171246@s.aou.edu.jo>
2021-06-20 17:04:08 +0300

    Created Data Structures Repo

---
+++ code-challenges/bin/clean-up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Cleaning up old directory"
+
+yes | rm -fr dsa-template
+
+echo 'Done'
---
+++ code-challenges/bin/shift-dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Shiftting Directories"
+
+
+mv -f ./* ../
+
+cd ..
+
+echo 'Done'
