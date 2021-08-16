38d8ac040b71142282397f239dddcad40f5bab9f
qasem-moh <2171246@s.aou.edu.jo>
2021-08-16 17:00:14 +0300

    ch07

---
+++ code-challenges-401/linked-list-kth/README.md
@@ -0,0 +1,18 @@
+# Linked List
+
+[**Linked List**](./linked-list.js)
+
+**Challenge**:
+- Create a linked list class which has methods to `insert at head`, `find a node by value` and `print a collection` of all the nodes in the linked list.
+- In current linked list, create a method to `append` in the end, a method to `insert after` a value, and another method to `insert before` value.
+- In current linked list, create a method to `get k-th value` from the end... this was made easy by integrating a length counter on the linked list.
+- In current linked list, create a function `zipLists` which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list.
+
+**Approach & Efficiency**: A class was created along with the methods... a helper function, `fromValues` was created to help in inserting multiple values at once for testing purposes and whatnot.
+
+**Solution**:
+
+*run: npm test linked-list*
+
+
+![](https://i.ibb.co/sgJ3cV4/5555.png)
---
+++ code-challenges-401/linked-list-kth/ll-kth-from-end.test.js
@@ -0,0 +1,53 @@
+'use strict';
+const lL = require('./ll-kth-from-end');
+
+let newLl = new lL();
+
+
+let value = 1;
+let value2 = 2;
+let value3 = 3;
+let value4 = 4;
+let value5 = 5;
+let value6 = 6;
+
+describe('Testing kthFromEnd(k)', () => {
+    it('k is greater than the length of the linked list', () => {
+        newLl.append(value);
+        newLl.append(value2);
+        newLl.append(value3);
+        newLl.append(value4);
+        newLl.append(value5);
+        newLl.append(value6);
+        expect(newLl.kthFromEnd(7)).toEqual('Exception');
+    });
+
+    it('k and the length of the list are the same', () => {
+        expect(newLl.kthFromEnd(6)).toEqual('Exception');
+    });
+    it('k is not a positive integer', () => {
+        expect(newLl.kthFromEnd(-2)).toEqual('K Not Positive');
+    });
+    it('linked list is of a size 1', () => {
+        let newLl2 = new lL();
+        newLl2.append(8);
+        expect(newLl2.kthFromEnd(2)).toEqual('Exception');
+        expect(newLl2.kthFromEnd(1)).toEqual('Exception');
+        expect(newLl2.kthFromEnd(0)).toEqual(8);
+    });
+
+    it('Happy Path', () => {
+        let newLl = new lL();
+
+        newLl.append(value);
+        newLl.append(value2);
+        newLl.append(value3);
+        newLl.append(value4);
+        newLl.append(value5);
+        newLl.append(value6);
+        let value7 = 7;
+        newLl.append(value7);
+        expect(newLl.kthFromEnd(3)).toEqual('Happy Path');
+    });
+
+});
