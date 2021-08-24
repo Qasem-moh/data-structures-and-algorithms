74bec708adfa968077bea959e5b05bd43913239b
qasem-moh <2171246@s.aou.edu.jo>
2021-08-24 17:00:26 +0300

    ch13

---
+++ code-challenges-401/stack-queue-brackets/multi-bracket-validation.js
@@ -0,0 +1,45 @@
+/* eslint-disable indent */
+'use strict';
+
+const Stack = require('./stacks-and-queues').Stack;
+
+function multiBracketValidation(string) {
+    if (!string) return false;
+
+    let bracketsArray = [];
+    let split = string.split('');
+    for (let i = 0; i < split.length; i++) {
+        if (split[i] === '(') bracketsArray.push(1);
+        if (split[i] === ')') bracketsArray.push(-1);
+        if (split[i] === '{') bracketsArray.push(2);
+        if (split[i] === '}') bracketsArray.push(-2);
+        if (split[i] === '[') bracketsArray.push(3);
+        if (split[i] === ']') bracketsArray.push(-3);
+    }
+    if (bracketsArray.length <= 1) return false;
+    if (bracketsArray[0] < 0) return false;
+
+    const stack = new Stack();
+
+    let length = bracketsArray.length;
+
+    for (let i = 0; i < length; i++) {
+        let bracket = bracketsArray.shift();
+        console.log('before', stack.top)
+        if (!stack.top) {
+            stack.push(bracket);
+        } else if (bracket > 0) {
+            stack.push(bracket);
+        } else if ((bracket * -1) === stack.top.value) { 
+            stack.pop();
+        } else {
+            return false;
+        }
+        console.log('after', stack.top)
+    }
+    return true;
+}
+
+module.exports = {
+    multiBracketValidation
+};
